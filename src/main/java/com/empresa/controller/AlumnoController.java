dd0251f2b323f9789843ec8eabc76e8f9a3cc80f
Eiron616 <aaron_fr999@hotmail.com>
2024-07-25 17:33:05 -0500

    actualizar

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,18 @@ public class AlumnoController {
 	@PostMapping
 	public ResponseEntity<Alumno> registra(@RequestBody Alumno obj){
 		System.out.println(">>registro " +obj.getIdAlumno());
+		Alumno objSalida=service.insertaActualizaAlumno(obj);
+		if (objSalida!=null) {
+			return ResponseEntity.ok(objSalida);
+		}
+		else {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	@PutMapping
+	public ResponseEntity<Alumno> actualiza(@RequestBody Alumno obj){
+		System.out.println(">>actualiza " +obj.getIdAlumno());
 		Alumno objSalida=service.insertaActualizaAlumno(obj);
 		if (objSalida!=null) {
 			return ResponseEntity.ok(objSalida);
